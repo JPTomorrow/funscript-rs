4ed7e43dd2b6dfcc13468db1d79987a8e48a1414
JPTomorrow <justin.parker.morrow@gmail.com>
2023-02-05 20:23:14 -0700

    json is now serialized to structures and save feature

--- src/funscript.rs
+++ src/funscript.rs
@@ -1,7 +1,47 @@
+use serde::{Deserialize, Serialize};
 use serde_json::{Error as SerdeError, Value};
 use thiserror::Error;
 
-pub type FScript = Value; // empty named tuple for json
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct FSPoint {
+    pub pos: i32,
+    pub at: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct SimulatorPresets {
+    pub name: String,
+    pub full_range: bool,
+    pub direction: i32,
+    pub rotation: f32,
+    pub length: f32,
+    pub width: f32,
+    pub offset: String,
+    pub color: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct FScript {
+    pub version: String,
+    pub inverted: bool,
+    pub range: i32,
+    pub bookmark: i32,
+    pub last_position: i64,
+    pub graph_duration: i32,
+    pub speed_ratio: f32,
+    pub injection_speed: i32,
+    pub injection_bias: f32,
+    pub scripting_mode: i32,
+    pub simulator_presets: Vec<SimulatorPresets>,
+    pub active_simulator: i32,
+    pub reduction_tolerance: f32,
+    pub reduction_stretch: f32,
+    pub clips: Vec<Value>,
+    pub actions: Vec<FSPoint>,
+    pub raw_actions: Vec<FSPoint>,
+}
 
 #[derive(Error, Debug)]
 pub enum FunscriptError {
@@ -13,8 +53,21 @@ pub enum FunscriptError {
 
 pub fn load_funscript(path: &str) -> Result<FScript, FunscriptError> {
     let file = std::fs::read_to_string(path)?;
-    let json = serde_json::from_str::<Value>(&file)?;
+    let json = serde_json::from_str::<FScript>(&file)?;
     Ok(json)
+}
+
+pub fn save_funscript(path: &str, script: &FScript) -> Result<(), FunscriptError> {
+    if !path.ends_with(".funscript") {
+        return Err(FunscriptError::FileReadError(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "invalid file extension",
+        )));
+    }
+
+    let json = serde_json::to_string_pretty(script)?;
+    std::fs::write(path, json)?;
+    Ok(())
 }
 
 pub fn print_script(script: &FScript) {
@@ -36,5 +89,17 @@ mod tests {
         } else {
             assert_matches!(s, Ok(_));
         }
+    }
+
+    #[test]
+    fn test_save_funscript() {
+        let path = "./test-scripts/joyfunscripter.funscript";
+        let save_path = "./test-scripts/out/joyfunscripter.funscript";
+
+        let mut s = load_funscript(path).unwrap();
+        s.bookmark = 100000;
+        save_funscript(save_path, &s).unwrap();
+        let check = load_funscript(save_path).unwrap();
+        assert_eq!(check.bookmark, 100000);
     }
 }
