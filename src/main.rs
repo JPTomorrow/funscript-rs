c272b39edcc515b79d3af4ca2117b67dfccd7bc7
JPTomorrow <justin.parker.morrow@gmail.com>
2023-02-20 17:02:52 -0700

    trying to remove native-dialog

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,25 @@
 mod funscript;
 
 use crate::funscript::*;
-use native_dialog::FileDialog;
+// use native_dialog::FileDialog;
 
 fn main() {
-    let path = FileDialog::new()
-        .set_location("~/")
-        .add_filter("Funscript Files", &["funscript"])
-        .show_open_single_file()
-        .unwrap();
+    // let path = FileDialog::new()
+    //     .set_location("~/")
+    //     .add_filter("Funscript Files", &["funscript"])
+    //     .show_open_single_file()
+    //     .unwrap();
 
-    let path = match path {
-        Some(path) => path,
-        None => panic!("no file selected!"),
-    };
+    // let path = match path {
+    //     Some(path) => path,
+    //     None => panic!("no file selected!"),
+    // };
 
-    let path_str = path.to_str().unwrap();
+    // let path_str = path.to_str().unwrap();
 
-    let mut s: FScript = load_funscript(path_str).expect("failed to load script");
-    print_script_diagnostics(&s);
-    apply_rdp(&mut s, 10.0);
-    print_script_diagnostics(&s);
-    save_funscript(path_str, &s).expect("failed to save script");
+    // let mut s: FScript = load_funscript(path_str).expect("failed to load script");
+    // print_script_diagnostics(&s);
+    // apply_rdp(&mut s, 10.0);
+    // print_script_diagnostics(&s);
+    // save_funscript(path_str, &s).expect("failed to save script");
 }
